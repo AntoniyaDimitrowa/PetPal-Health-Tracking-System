e1bf8f1b5f391ef4f11b27cf07cff4cb8525ef46
Antonia Dimitrova <a.dimitrova@student.fontys.nl>
2024-09-25 13:52:35 +0200

    Health service implementation

--- src/main/java/com/example/petpal/business/impl/HealthServiceImpl.java
+++ src/main/java/com/example/petpal/business/impl/HealthServiceImpl.java
@@ -1,18 +1,25 @@
 package com.example.petpal.business.impl;
 
 import com.example.petpal.business.IHealthService;
+import com.example.petpal.business.converters.HealthRecordConverter;
 import com.example.petpal.business.domain.HealthRecord;
+import com.example.petpal.persistence.IPetRepository;
 
 import java.util.ArrayList;
 
 public class HealthServiceImpl implements IHealthService {
+    private final IPetRepository petRepository;
+
+    public HealthServiceImpl(IPetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
     @Override
     public void addHealthRecord(long petId, HealthRecord healthRecord) {
-
+        petRepository.addHealthRecordToPet(petId, HealthRecordConverter.convertFromHealthRecordToHealthRecordEntity(healthRecord));
     }
 
     @Override
     public ArrayList<HealthRecord> getHealthRecordsByPetId(long petId) {
-        return null;
+        return HealthRecordConverter.convertFromHealthRecordEntitiesToHealthRecords(petRepository.getHealthRecordsByPetId(petId));
     }
 }
