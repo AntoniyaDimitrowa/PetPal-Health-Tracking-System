62b58870b1281db76fe8354bc0708de49aeba8af
Antonia Dimitrova <a.dimitrova@student.fontys.nl>
2024-09-26 00:47:54 +0200

    Adding tests
    Adding classes for testing Vaccination and Health Services:
    VaccinationServiceImplTest
    HealthServiceImplTest

--- src/main/java/com/example/petpal/persistence/entity/HealthRecordEntity.java
+++ src/main/java/com/example/petpal/persistence/entity/HealthRecordEntity.java
@@ -3,12 +3,14 @@ package com.example.petpal.persistence.entity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class HealthRecordEntity {
     private long id;
     private Date date;
